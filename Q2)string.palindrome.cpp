60270db4b46fac30a799185baf3a7ff9ef4493ba
Sagnik Bose <sagnikbose2006@gmail.com>
2026-09-05 13:04:34 +0530

    Add files via upload

---
+++ Q2)string.palindrome.cpp
@@ -0,0 +1,36 @@
+// This program checks if a given string is a palindrome or not.
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char string1[20];
+    int i, length;
+    int flag = 0;
+
+    cout << "Enter a string: ";
+    cin >> string1;
+
+    length = strlen(string1);
+
+    for (i = 0; i < length; i++)
+    {
+        if (string1[i] != string1[length - i - 1])
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag != 0)
+    {
+        cout << string1 << " is not a palindrome" << endl;
+    }
+    else
+    {
+        cout << string1 << " is a palindrome" << endl;
+    }
+
+    return 0;
+}
